6ae6fbc118519464d1e70b918085dade3a12191d
megh-bari <meghbari01@gmail.com>
2024-06-28 16:44:40 +0530

    string

---
+++ Javascript/Lec 5/string.js
@@ -0,0 +1,43 @@
+// Strings => sequence of characters
+
+// declartion of srtring in diff method
+
+let firstName = "Megh"
+
+console.log(typeof(firstName));
+
+let userName = `user
+is 
+not 
+found` //template literals or backticks
+
+console.log(userName);
+
+let stringName = new String("Megh Bari")
+console.log(stringName);
+
+
+// string operation 
+
+let lang1 = "English "
+let lang2 = "Hindi"
+
+// let ans = lang1 + lang2
+// console.log(ans);
+
+// let ans = `${lang1} ${lang2}`
+// console.log(ans);
+
+// console.log(lang1.length);
+
+// console.log(lang1.toUpperCase());
+// console.log(lang2.toLowerCase());
+
+
+// console.log(lang1.substring(3));
+// console.log(lang1.substring(2,5));
+
+
+let sentance = "Hello-jee-kaise-ho-saare "
+console.log(sentance.split('-'));
+
